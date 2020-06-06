51b6bb35a04abc01f03a025f624031eec5dba8a5
Maximilian Beck <contact@maximilianbeck.de>
2020-06-06 19:54:33 +0200

    #2: Expect json if no Accept header is set

--- address-api/app/Http/Kernel.php
+++ address-api/app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ExpectJson;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
+            ExpectJson::class,
             // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
