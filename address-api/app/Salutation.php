98f882d422825613bf9ad6bc2402731b1d7ae52d
Maximilian Beck <contact@maximilianbeck.de>
2020-06-06 14:58:38 +0200

    #2: Add salutations table and model

---
+++ address-api/app/Salutation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Salutation
+ *
+ * @property string $id
+ * @property string $key
+ * @property string $created_at
+ * @property string $updated_at
+ * @mixin \Eloquent
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation whereKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Salutation whereUpdatedAt($value)
+ */
+class Salutation extends Model
+{
+}
