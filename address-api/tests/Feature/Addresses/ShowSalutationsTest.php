cd77605c03795a222b18693ee4b2f2cb43572132
Maximilian Beck <contact@maximilianbeck.de>
2020-06-06 17:26:56 +0200

    #2: Add api test for showing all salutations

---
+++ address-api/tests/Feature/Addresses/ShowSalutationsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Addresses;
+
+use Tests\TestCase;
+
+class ShowSalutationsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testShowAllSalutations(): void
+    {
+        $response = $this->get('/api/v1/salutations');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(2, 'data');
+        $response->assertJsonStructure(['data' => ['id', 'key']]);
+    }
+}
