a483d621b27e64e027cb0d7eb47f71616aef516f
Maximilian Beck <contact@maximilianbeck.de>
2020-06-07 11:42:38 +0200

    #4: Add material-ui and verify installation with default navbar

--- address-frontend/src/app/app.module.ts
+++ address-frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppUiModule } from './ui-components/app-ui.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, AppUiModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ address-frontend/src/app/ui-components/app-ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+    imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule, MatSidenavModule],
+    declarations: [NavBarComponent],
+    exports: [NavBarComponent],
+})
+export class AppUiModule {}
