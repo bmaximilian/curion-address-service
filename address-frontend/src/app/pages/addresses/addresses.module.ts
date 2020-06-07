9b5946263b1af39a26d3548adbc6ab3ac3de8458
Maximilian Beck <contact@maximilianbeck.de>
2020-06-07 16:23:19 +0200

    #4: Connect new services in address module

--- address-frontend/src/app/pages/addresses/addresses.module.ts
+++ address-frontend/src/app/pages/addresses/addresses.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { AddressesPageComponent } from './addresses-page/addresses-page.component';
+import { addressesReducer } from './store/reducer/addresses.reducer';
+import { AddressesService } from './addresses.service';
+import { AddressEffects } from './store/effects/address.effects';
 
 @NgModule({
+    imports: [StoreModule.forFeature('addresses', addressesReducer), EffectsModule.forFeature([AddressEffects])],
+    providers: [AddressesService],
     declarations: [AddressesPageComponent],
 })
 export class AddressesModule {}
