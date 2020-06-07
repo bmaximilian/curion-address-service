031afe70f3e969cd60d14515c7239a5e0eeeb8d6
Maximilian Beck <contact@maximilianbeck.de>
2020-06-07 22:39:59 +0200

    #7: Fix tests

--- address-frontend/src/app/pages/addresses/components/address-form-dialog/address-form-dialog.component.spec.ts
+++ address-frontend/src/app/pages/addresses/components/address-form-dialog/address-form-dialog.component.spec.ts
@@ -1,4 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { StoreModule } from '@ngrx/store';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { addressesReducer } from '../../store/reducer/addresses.reducer';
 import { AddressFormDialogComponent } from './address-form-dialog.component';
 
 describe('AddressFormDialogComponent', () => {
@@ -7,6 +17,21 @@ describe('AddressFormDialogComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [
+                NoopAnimationsModule,
+                StoreModule.forRoot({ addresses: addressesReducer }),
+                TranslateModule.forRoot(),
+                MatDialogModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatSelectModule,
+                MatDatepickerModule,
+                MatNativeDateModule,
+            ],
+            providers: [
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+            ],
             declarations: [AddressFormDialogComponent],
         }).compileComponents();
     }));
