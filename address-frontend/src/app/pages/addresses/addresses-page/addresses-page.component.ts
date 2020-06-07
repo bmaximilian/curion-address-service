59241e4a399eed7b7f43a23cc9ec3ee5d1a3a4ee
Maximilian Beck <contact@maximilianbeck.de>
2020-06-07 16:21:19 +0200

    #4: Load addresses when the page component is created

--- address-frontend/src/app/pages/addresses/addresses-page/addresses-page.component.ts
+++ address-frontend/src/app/pages/addresses/addresses-page/addresses-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { loadAddressesStart } from '../store/actions/address-list.actions';
 
 @Component({
     selector: 'app-addresses-page',
     templateUrl: './addresses-page.component.html',
     styleUrls: ['./addresses-page.component.scss'],
 })
-export class AddressesPageComponent {}
+export class AddressesPageComponent {
+    /**
+     * Constructor of Addresses page
+     *
+     * @param store - The store service
+     */
+    constructor(private store: Store) {
+        store.dispatch(loadAddressesStart());
+    }
+}
