a3c9bb69786dec62c0ba8a23ef287871cd2e80fa
Maximilian Beck <contact@maximilianbeck.de>
2020-06-10 17:24:38 +0200

    #7: Display salutations dynamically

--- address-frontend/src/app/pages/addresses/components/address-form-dialog/address-form-dialog.component.ts
+++ address-frontend/src/app/pages/addresses/components/address-form-dialog/address-form-dialog.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { SalutationItem } from '../../salutation.model';
+import { State } from '../../store/reducer/addresses.reducer';
 
 export interface DialogData {
     title: string;
@@ -13,6 +17,8 @@ export interface DialogData {
     styleUrls: ['./address-form-dialog.component.scss'],
 })
 export class AddressFormDialogComponent {
+    public $salutations: Observable<SalutationItem[]>;
+
     public addressForm = new FormGroup({
         salutation: new FormControl(''),
         firstName: new FormControl(''),
@@ -28,11 +34,15 @@ export class AddressFormDialogComponent {
      *
      * @param dialogRef - Reference to the dialog instance
      * @param data - The passed dialog dta
+     * @param store - The application store of ngrx
      */
     constructor(
         public dialogRef: MatDialogRef<AddressFormDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    ) {}
+        public store: Store<{ addresses: State }>,
+    ) {
+        this.$salutations = store.pipe(select((state) => state.addresses.salutations.items));
+    }
 
     /**
      * Click on the submit button
